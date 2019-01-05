d2c59f1296f479eb7606d3da57c0fda7195ace96
Viktor Carlén <vibo@Viktors-MacBook-Pro.local>
2019-01-05 13:52:45 +0100

    Setup bundler

---
+++ src/sharedStyles.js
@@ -0,0 +1,4 @@
+export const styles = `
+  --vibo-primary: #0000a0;
+  --vibo-secondary: #FF0000;
+`
---
+++ src/vibo-button/index.js
@@ -0,0 +1,9 @@
+import {LitElement, html} from '@polymer/lit-element'
+
+export class ViboButton extends LitElement {
+  render() {
+    return html`
+      <span></span>
+    `
+  }
+}
